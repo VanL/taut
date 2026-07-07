c73e299403f07001befb08322b7608dbb1cabb5f
VanL <van@ospo.co>
2026-07-06 19:26:41 -0500

    Update version

--- taut/_constants.py
+++ taut/_constants.py
@@ -17,7 +17,7 @@ from typing import Any, Final
 
 from simplebroker import resolve_config
 
-__version__: Final[str] = "0.4.6"
+__version__: Final[str] = "0.4.7"
 
 DEFAULT_DB_NAME: Final[str] = ".taut.db"
 PROJECT_CONFIG_NAME: Final[str] = ".taut.toml"
